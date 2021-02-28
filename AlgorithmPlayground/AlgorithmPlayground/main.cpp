717ba323605de67c2e0284408b8c37a74ef3696b
denny.k <denny.k@kakaocorp.com>
2021-02-28 14:27:15 +0900

    Leet Code : Two Sum

--- AlgorithmPlayground/AlgorithmPlayground/main.cpp
+++ AlgorithmPlayground/AlgorithmPlayground/main.cpp
@@ -6,9 +6,25 @@
 //
 
 #include <iostream>
+#include <vector>
+
+std::vector<int> twoSum(std::vector<int>& nums, int target);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
+}
+
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    for(int i = 0; i < nums.size(); ++i) {
+        for (int j = i + 1; j < nums.size(); ++j) {
+            if(nums[i] + nums[j] == target) {
+                std::vector<int> result = {i, j};
+                return result;
+            }
+        }
+    }
+    
+    std::vector<int> sampleResult = {0, 0};
+    return sampleResult;
 }
