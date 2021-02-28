00ef123d900eaf2e834bdf3909ac3e9d268d611b
denny.k <denny.k@kakaocorp.com>
2021-02-28 14:31:48 +0900

    LeetCode - Add Two Numbers

--- AlgorithmPlayground/AlgorithmPlayground/main.cpp
+++ AlgorithmPlayground/AlgorithmPlayground/main.cpp
@@ -7,12 +7,41 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-std::vector<int> twoSum(std::vector<int>& nums, int target);
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 int main(int argc, const char * argv[]) {
     
     return 0;
+}
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    int sum = 0;
+    ListNode *l3 = NULL;
+    ListNode **node = &l3;
+    
+    while(l1 != NULL || l2 != NULL || sum > 0) {
+        if(l1 != NULL) {
+            sum += l1 -> val;
+            l1 = l1 -> next;
+        }
+        if(l2 != NULL) {
+            sum += l2 -> val;
+            l2 = l2 -> next;
+        }
+        
+        (*node) = new ListNode(sum % 10);
+        sum /= 10;
+        node = &((*node) -> next);
+    }
+    return l3;
 }
 
 std::vector<int> twoSum(std::vector<int>& nums, int target) {
